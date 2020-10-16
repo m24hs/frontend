ad5a2f2ff584be7c7307bb079d01dfdd09f9f3a7
Marcelo Rossini <mar.18.celo@gmail.com>
2020-10-16 10:21:12 -0300

    upload

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+    baseURL: "https://marcelorossini-com-br.umbler.net/",
     timeout: 60000,
 });
 export default api;
