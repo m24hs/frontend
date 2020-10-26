b15ff3b0155ce32e993f80e959543ec9a366fd80
Marcelo Rossini <mar.18.celo@gmail.com>
2020-10-26 17:07:10 -0300

    upload

--- src/pages/servicos/index.js
+++ src/pages/servicos/index.js
@@ -32,88 +32,6 @@ const getData = async () =>
       data: null,
     }));
 
-const Servicos = ({ error, data }) => {
-  // Variáveis auxiliares
-  const router = useRouter();
-  const path = process.env.NEXT_PUBLIC_SERVER_URL;
-
-  return (
-    <>
-      <Head>
-        <title>Serviços - M24</title>
-      </Head>
-      <Layout>
-        <Container>
-          <PageTitle>Serviços</PageTitle>
-          {error && <div>Erro</div>}
-          <ListService>
-            {!error &&
-              data.map((item, index) => (
-                <>
-                  <ListItem key={index} right={index % 2 !== 0}>
-                    <span>{index !== 0 && <Divider />}</span>
-                    <PageTitle small>{item.title}</PageTitle>
-                    <ListImage>
-                      <img src={path + item.image} />
-                    </ListImage>
-                    <ViewHtml
-                      dangerouslySetInnerHTML={{ __html: item.description }}
-                    />
-                    <Link href={`${router.pathname}/${item.url}`}>
-                      <Button secondary>Saiba mais</Button>
-                    </Link>
-                  </ListItem>
-                </>
-              ))}
-          </ListService>
-        </Container>
-      </Layout>
-    </>
-  );
-};
-
-Servicos.getInitialProps = async (ctx) => {
-  const servicos = await getData();
-  return servicos;
-}
-
-export default Servicos;
-
-/*
-// Imports padrão
-import { useState, useEffect } from "react";
-import Head from "next/head";
-import Link from "next/link";
-import { useRouter } from "next/router";
-
-// Imports de estilo
-import Layout from "../../components/site/Layout";
-import { Divider, PageTitle, Button, ViewHtml } from "../../styles/global";
-import {
-  Container,
-  ListService,
-  ListItem,
-  ListImage,
-} from "../../styles/pages/servicos/index.js";
-
-// Imports auxiliares
-import api from "../../services/api";
-
-// Carrega data
-const getData = async () =>
-  await api
-    .get("/services/", {
-      params: { columns: ["title", "description", "image", "url"] },
-    })
-    .then((res) => ({
-      error: false,
-      data: res.data,
-    }))
-    .catch(() => ({
-      error: true,
-      data: null,
-    }));
-
 export async function getServerSideProps(context) {
   const servicos = await getData();
   return {
@@ -162,4 +80,3 @@ const Servicos = ({ error, data }) => {
 };
 
 export default Servicos;
-*/
