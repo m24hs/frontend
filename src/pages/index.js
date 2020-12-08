cd0afc4702ff5dc7e73d3fd31e50a78fe89ffa23
Vitória Ketillin Fusco <vitoriaketillin@hotmail.com>
2020-12-07 22:32:55 -0300

    remove Import

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ import Link from "next/link";
 import Layout from "../components/site/Layout";
 import { Button } from "../styles/global";
 import { Container, Main, Label, VideoWrapper } from "../styles/pages/index";
-import Background from "../assets/home/bannerInicio.jpeg";
 
 // Imports Auxiliares
 import api from "../services/api";
